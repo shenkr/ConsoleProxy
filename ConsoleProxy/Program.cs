e5f950c66048632b530eeb2f574e68c89d4c61ac
shenkr <bohdan.jax@gmail.com>
2018-04-23 10:08:35 +0300

    first commit

---
+++ ConsoleProxy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ConsoleProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://ip.jsontest.com/");
+            WebProxy myproxy = new WebProxy("36.37.134.18", 8080);
+            myproxy.BypassProxyOnLocal = false;
+            request.Proxy = myproxy;
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Hey Kappa");
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+
+            Console.WriteLine(responseFromServer);
+            Console.ReadLine();
+        }
+    }
+}
